575b8666242bca69b5866f65b325e76ebf3b8b77
Madeline King <madeline.king330@gmail.com>
2023-02-14 02:43:28 -0800

    dynamically render movie info from TMDb api

--- client/src/components/Entry.tsx
+++ client/src/components/Entry.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 type Binary = 0 | 1;
@@ -8,23 +8,41 @@ interface MovieProps {
   toggle: any;
 }
 
-const handleTitleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-  const title: string = e.currentTarget.getAttribute('data-title');
-  console.log('title', title);
-  axios.get('/api/movies/info', { params: { title } })
-    .then((res) => {
-      console.log('successful retrieval of movies')
-    })
-    .catch((err) => {
-      console.log('failure to retrieve movies')
-    })
-}
-
 const Entry = ({ entry, toggle }: MovieProps): JSX.Element => {
+  const [overview, setOverview] = useState<string | null>(null);
+  const [poster, setPoster] = useState<string | null>(null);
+  const [release, setRelease] = useState<string | null>(null);
+  const [runtime, setRuntime] = useState<number | null>(null);
+  const [vote, setVote] = useState<number | null>(null);
+
+  const handleTitleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const title: string = e.currentTarget.getAttribute('data-title');
+    console.log('title', title);
+    axios.get('/api/movies/info', { params: { title } })
+      .then((res: any) => {
+        console.log('successful retrieval of movies', res.data)
+        setOverview(res.data.overview);
+        setPoster(res.data.poster_path);
+        setRelease(res.data.release_date);
+        setRuntime(res.data.runtime);
+        setVote(res.data.vote_average);
+      })
+      .catch((err) => {
+        console.log('failure to retrieve movies')
+      })
+  }
+
   return (
     <div>
       <div data-title={entry.title} onClick={ handleTitleClick.bind(this) }>{entry.title}
         <button onClick={() => toggle(entry)}>{entry.watched === 0 ? 'To Watch' : 'Watched'}</button>
+        <div id="movie-container">
+          {overview !== null ? <div id="movie-overview">Summary: {overview}</div> : null}
+          {poster !== null ? <img id="movie-img" src={poster} alt=''></img> : null}
+          {release !== null ? <div id="movie-release">Release Date: {release}</div> : null}
+          {runtime !== null ? <div id="movie-runtime">Runtime: {runtime}</div> : null}
+          {vote !== null ? <div id="movie-vote">Rating Average: {vote}</div> : null}
+        </div>
       </div>
     </div>
   )
--- server/controllers/controllers.ts
+++ server/controllers/controllers.ts
@@ -4,6 +4,7 @@ import { searchMovie, getMovie } from '../helpers/tmdb';
 
 interface MovieInfo {
   overview: string,
+  poster_path: string,
   release_date: string,
   runtime: number,
   vote_average: number,
@@ -17,6 +18,7 @@ export const controllers = {
       .then((result: any) => {
         const movieInfo: MovieInfo = {
           overview: result.data.overview,
+          poster_path: `https://image.tmdb.org/t/p/original/${result.data.poster_path}`,
           release_date: result.data.release_date,
           runtime: result.data.runtime,
           vote_average: result.data.vote_average,
@@ -25,12 +27,10 @@ export const controllers = {
         res.status(200).send(movieInfo);
       })
       .catch((err: unknown) => {
-        console.log('error inside getMovie', err);
         res.status(400).send();
       })
     })
     .catch((err: unknown) => {
-      console.log('error inside searchMovie');
       res.status(400).send();
     })
   }
